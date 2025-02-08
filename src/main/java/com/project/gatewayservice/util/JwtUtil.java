dec6d57e67c4cc9ad7e746643d71da045377b964
NMWXask <k.marcus@mail.ru>
2025-02-08 17:52:47 +0400

    убрал секрет в проперти, все отрабатывает корректно

--- src/main/java/com/project/gatewayservice/util/JwtUtil.java
+++ src/main/java/com/project/gatewayservice/util/JwtUtil.java
@@ -1,24 +1,26 @@
 package com.project.gatewayservice.util;
 
+import com.project.gatewayservice.config.JwtConfig;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 import java.security.Key;
 @Component
+@RequiredArgsConstructor
 public class JwtUtil {
 
-    private static final String SECRET_KEY = "E38FAC7A1631103FA4A4670A8749EE01062B937DC639E88F933BD3691CA0C116";
+    private final JwtConfig jwtConfig;
+
     public void validateToken(final String token) {
 
         Jwts.parserBuilder().setSigningKey(getSignKey()).build().parseClaimsJws(token);
     }
 
-
-
     private Key getSignKey() {
-        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        byte[] keyBytes = Decoders.BASE64.decode(jwtConfig.getSecret());
         return Keys.hmacShaKeyFor(keyBytes);
     }
 }
